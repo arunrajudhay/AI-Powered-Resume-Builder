72eb07cf7b3aefd1b9f84c7f77363d0a32cf729b
ARUNRAJ R U <158137731+arunrajudhay@users.noreply.github.com>
2025-07-23 17:13:34 +0530

    Update app.py
    Deleting file by clearing contents

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-from flask import Flask, render_template
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "hi"
-
-@app.route('/arunraj/<user_name>')
-def arun(user_name):
-    return render_template('index.html', name=user_name)
-
-if __name__ == '__main__':
-    app.run(debug=True)
